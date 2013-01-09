8c77aa65faf259029dc013448dd2c7ee510cb53e
Tommy Chen <tommy351@gmail.com>
2013-01-09 12:22:19 +0800

    Core test: New article

--- test/core.js
+++ test/core.js
@@ -5,8 +5,8 @@ var spawn = require('child_process').spawn,
   coreDir = __dirname + '/../';
 
 var regex = {
-  post: /---\nlayout: post\ntitle: (.*?)\ndate: \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\ncomments: (true|false)\ntags:(.*)?\n---\n[\s\S]+/,
-  page: /---\nlayout: post\ntitle: (.*?)\ndate: \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\ncomments: (true|false)\n---\n[\s\S]+/
+  post: /---\nlayout: post\ntitle: (.*?)\ndate: \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\ncomments: (true|false)\ntags:(.*)?\n---\n[\s\S]*/,
+  page: /---\nlayout: page\ntitle: (.*?)\ndate: \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\ncomments: (true|false)\n---\n[\s\S]*/
 };
 
 describe('Core', function(){
@@ -51,6 +51,28 @@ describe('Core', function(){
         results[4].should.equal(results[5]);
         results[6].should.match(regex.post);
         done();
+      });
+    });
+  });
+
+  describe('Create', function(){
+    it('post', function(done){
+      spawn('../bin/hexo', ['new_post', 'Test Post'], {cwd: coreDir + 'tmp'}).on('exit', function(){
+        file.read(coreDir + 'tmp/source/_posts/test-post.md', function(err, content){
+          if (err) throw err;
+          content.should.match(regex.post);
+          done();
+        });
+      });
+    });
+
+    it('page', function(done){
+      spawn('../bin/hexo', ['new_page', 'Test Page'], {cwd: coreDir + 'tmp'}).on('exit', function(err, content){
+        file.read(coreDir + 'tmp/source/test-page/index.md', function(err, content){
+          if (err) throw err;
+          content.should.match(regex.page);
+          done();
+        });
       });
     });
   });
