a5a9b329bf69cba3f493f090c61b942c6d06b5bd
Tommy Chen <tommy351@gmail.com>
2013-06-28 17:25:37 +0800

    Fixed menu controller

--- public/js/controllers/common/menu.js
+++ public/js/controllers/common/menu.js
@@ -1,10 +1,5 @@
 define(function(require, exports, module){
   return ['$scope', 'sharedData', function($scope, sharedData){    
     $scope.data = sharedData;
-
-    $scope.menu = [
-      {href: 'posts', title: 'Posts'},
-      {href: 'files', title: 'Files'}
-    ];
   }]
 });
