8e9c9cf49d156356e4ca7c1c6a80fee359e73ca7
Tommy Chen <tommy351@gmail.com>
2013-08-09 23:24:36 +0800

    Added raw tag test

--- test/tag.js
+++ test/tag.js
@@ -129,7 +129,11 @@ describe('Tags', function(){
   });
 
   describe('raw', function(){
-    //
+    var raw = require('../lib/plugins/tag/raw');
+
+    it('content', function(){
+      raw([], '123456789<b>strong</b>987654321').should.be.eql('123456789<b>strong</b>987654321');
+    });
   });
 
   describe('vimeo', function(){
