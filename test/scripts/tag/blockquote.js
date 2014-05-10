b4def0fe55a902899a2a8b6fc0217d2ba4bca0ab
Tommy Chen <tommy351@gmail.com>
2014-05-10 11:57:30 +0800

    Fix tests

--- test/scripts/tag/blockquote.js
+++ test/scripts/tag/blockquote.js
@@ -4,35 +4,39 @@ var cheerio = require('cheerio'),
 describe('Tag - blockquote', function(){
   var blockquote = require('../../../lib/plugins/tag/blockquote');
 
-  var raw = '123456 **bold** and *italic*';
+  var bq = function(args){
+    var result = blockquote(args.split(' '), '123456 **bold** and *italic*');
+
+    return result.replace(/<escape>(.*?)<\/escape>/g, '$1');
+  };
 
   it('author', function(){
-    var $ = cheerio.load(blockquote('John Doe'.split(' '), raw));
+    var $ = cheerio.load(bq('John Doe'));
 
     $('blockquote footer strong').html().should.eql('John Doe');
   });
 
   it('author + source', function(){
-    var $ = cheerio.load(blockquote('John Doe, A book'.split(' '), raw));
+    var $ = cheerio.load(bq('John Doe, A book'));
 
     $('blockquote footer strong').html().should.eql('John Doe');
     $('blockquote footer cite').html().should.eql('A book');
   });
 
   it('author + link', function(){
-    var $ = cheerio.load(blockquote('John Doe http://zespia.tw'.split(' '), raw));
+    var $ = cheerio.load(bq('John Doe http://zespia.tw'));
 
     $('blockquote footer strong').html().should.eql('John Doe');
     $('blockquote footer cite').html().should.eql('<a href="http://zespia.tw">zespia.tw/</a>');
 
-    var $ = cheerio.load(blockquote('John Doe http://zespia.tw/this/is/a/fucking/long/url'.split(' '), raw));
+    var $ = cheerio.load(bq('John Doe http://zespia.tw/this/is/a/fucking/long/url'));
 
     $('blockquote footer strong').html().should.eql('John Doe');
     $('blockquote footer cite').html().should.eql('<a href="http://zespia.tw/this/is/a/fucking/long/url">zespia.tw/this/is/a/fucking/&hellip;</a>');
   });
 
   it('author + link + title', function(){
-    var $ = cheerio.load(blockquote('John Doe http://zespia.tw My Blog'.split(' '), raw));
+    var $ = cheerio.load(bq('John Doe http://zespia.tw My Blog'));
 
     $('blockquote footer strong').html().should.eql('John Doe');
     $('blockquote footer cite').html().should.eql('<a href="http://zespia.tw">My Blog</a>')
