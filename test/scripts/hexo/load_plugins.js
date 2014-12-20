45ca0bf02b01389640adc9febeb2a5c5ed28df06
Tommy Chen <tommy351@gmail.com>
2014-12-20 23:51:46 +0800

    Only load plugins listed in package.json

--- test/scripts/hexo/load_plugins.js
+++ test/scripts/hexo/load_plugins.js
@@ -1,6 +1,7 @@
 var should = require('chai').should();
 var fs = require('hexo-fs');
 var pathFn = require('path');
+var Promise = require('bluebird');
 
 describe('Load plugins', function(){
   var Hexo = require('../../../lib/hexo');
@@ -19,12 +20,27 @@ describe('Load plugins', function(){
   function validate(path){
     var result = hexo._script_test;
 
-    result.filename = path;
-    result.dirname = pathFn.dirname(path);
-    result.module.id = path;
-    result.module.filename = path;
+    result.filename.should.eql(path);
+    result.dirname.should.eql(pathFn.dirname(path));
+    result.module.id.should.eql(path);
+    result.module.filename.should.eql(path);
 
     delete hexo._script_test;
+  }
+
+  function createPackageFile(){
+    var pkg = {
+      name: 'hexo-site',
+      version: '0.0.0',
+      private: true,
+      dependencies: {}
+    };
+
+    for (var i = 0, len = arguments.length; i < len; i++){
+      pkg.dependencies[arguments[i]] = '*';
+    }
+
+    return fs.writeFile(pathFn.join(hexo.base_dir, 'package.json'), JSON.stringify(pkg, null, '  '));
   }
 
   hexo.env.init = true;
@@ -39,9 +55,13 @@ describe('Load plugins', function(){
   });
 
   it('load plugins', function(){
-    var path = pathFn.join(hexo.plugin_dir, 'hexo-plugin-test', 'index.js');
+    var name = 'hexo-plugin-test';
+    var path = pathFn.join(hexo.plugin_dir, name, 'index.js');
 
-    return fs.writeFile(path, script).then(function(){
+    return Promise.all([
+      createPackageFile(name),
+      fs.writeFile(path, script)
+    ]).then(function(){
       return loadPlugins(hexo);
     }).then(function(){
       validate(path);
@@ -51,13 +71,23 @@ describe('Load plugins', function(){
 
   it('ignore plugins whose name is not started with "hexo-"', function(){
     var script = 'hexo._script_test = true';
-    var path = pathFn.join(hexo.plugin_dir, 'another-plugin', 'index.js');
+    var name = 'another-plugin';
+    var path = pathFn.join(hexo.plugin_dir, name, 'index.js');
 
-    return fs.writeFile(path, script).then(function(){
+    return Promise.all([
+      createPackageFile(name),
+      fs.writeFile(path, script)
+    ]).then(function(){
       return loadPlugins(hexo);
     }).then(function(){
       should.not.exist(hexo._script_test);
       return fs.unlink(path);
+    });
+  });
+
+  it('ignore plugins which are in package.json but not exist actually', function(){
+    return createPackageFile('hexo-plugin-test').then(function(){
+      return loadPlugins(hexo);
     });
   });
 
