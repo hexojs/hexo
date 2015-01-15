6ab2154f464abb501ddc0062c31b4b751bf0b2d1
Tommy Chen <tommy351@gmail.com>
2015-01-15 19:17:42 +0800

    Add tests for generate, render console

--- test/scripts/console/deploy.js
+++ test/scripts/console/deploy.js
@@ -8,7 +8,9 @@ describe('deploy', function(){
   var deploy = require('../../../lib/plugins/console/deploy').bind(hexo);
 
   before(function(){
-    return fs.mkdirs(hexo.public_dir);
+    return fs.mkdirs(hexo.public_dir).then(function(){
+      return hexo.init();
+    });
   });
 
   after(function(){
@@ -17,6 +19,7 @@ describe('deploy', function(){
 
   it('single deploy setting', function(){
     var executed = 0;
+    var emitted = 0;
 
     hexo.config.deploy = {
       type: 'foo',
@@ -33,8 +36,17 @@ describe('deploy', function(){
       executed++;
     });
 
+    hexo.once('deployBefore', function(){
+      emitted++;
+    });
+
+    hexo.once('deployAfter', function(){
+      emitted++;
+    });
+
     return deploy({foo: 'foo', bar: 'bar'}).then(function(){
       executed.should.eql(1);
+      emitted.should.eql(2);
     });
   });
 
@@ -61,9 +73,24 @@ describe('deploy', function(){
 
   it('deployer not found');
 
-  it('-g/--generate argument');
+  it('generate', function(){
+    return fs.writeFile(pathFn.join(hexo.source_dir, 'test.txt'), 'test').then(function(){
+      return deploy({generate: true});
+    }).then(function(){
+      return fs.readFile(pathFn.join(hexo.public_dir, 'test.txt'));
+    }).then(function(content){
+      content.should.eql('test');
+      return fs.rmdir(hexo.source_dir);
+    });
+  });
 
-  it('run generate if public directory not exist');
-
-  it('deployBefore & deployAfter events');
+  it('run generate if public directory not exist', function(){
+    return fs.rmdir(hexo.public_dir).then(function(){
+      return deploy({});
+    }).then(function(){
+      return fs.exists(hexo.public_dir);
+    }).then(function(exist){
+      exist.should.be.true;
+    });
+  });
 });
