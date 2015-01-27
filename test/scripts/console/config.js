b8836dbccda9d1df349653e1ee6671717b6b56c7
Tommy Chen <tommy351@gmail.com>
2015-01-27 22:55:33 +0800

    Add JSON support to "hexo config" command

--- test/scripts/console/config.js
+++ test/scripts/console/config.js
@@ -68,4 +68,29 @@ describe('config', function(){
       should.not.exist(config.language);
     });
   });
+
+  it('write config: json', function(){
+    var configPath = hexo.config_path = pathFn.join(hexo.base_dir, '_config.json');
+
+    return fs.writeFile(configPath, '{}').then(function(){
+      return config({_: ['title', 'My Blog']});
+    }).then(function(){
+      return fs.readFile(configPath);
+    }).then(function(content){
+      var json = JSON.parse(content);
+
+      json.title.should.eql('My Blog');
+
+      hexo.config_path = pathFn.join(hexo.base_dir, '_config.yml');
+      return fs.unlink(configPath);
+    });
+  });
+
+  it('create config if not exist', function(){
+    return fs.unlink(hexo.config_path).then(function(){
+      return writeConfig('subtitle', 'Hello world');
+    }).then(function(config){
+      config.subtitle.should.eql('Hello world');
+    });
+  });
 });
