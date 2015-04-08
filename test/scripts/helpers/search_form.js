8812b6171b6c8e368d867803922317c5b2fcfac8
Jamie Humphries <jamie.humphries@softwire.com>
2015-04-08 11:05:34 +0100

    Fix the tests for #1185 fix.

--- test/scripts/helpers/search_form.js
+++ test/scripts/helpers/search_form.js
@@ -10,21 +10,21 @@ describe('search_form', function(){
   it('default', function(){
     searchForm().should.eql('<form action="//google.com/search" method="get" accept-charset="UTF-8" class="search-form">' +
       '<input type="search" name="q" results="0" class="search-form-input" placeholder="Search">' +
-      '<input type="hidden" name="q" value="site:http://hexo.io">' +
+      '<input type="hidden" name="sitesearch" value="http://hexo.io">' +
       '</form>');
   });
 
   it('class', function(){
     searchForm({class: 'foo'}).should.eql('<form action="//google.com/search" method="get" accept-charset="UTF-8" class="foo">' +
       '<input type="search" name="q" results="0" class="foo-input" placeholder="Search">' +
-      '<input type="hidden" name="q" value="site:http://hexo.io">' +
+      '<input type="hidden" name="sitesearch" value="http://hexo.io">' +
       '</form>');
   });
 
   it('text', function(){
     searchForm({text: 'Find'}).should.eql('<form action="//google.com/search" method="get" accept-charset="UTF-8" class="search-form">' +
       '<input type="search" name="q" results="0" class="search-form-input" placeholder="Find">' +
-      '<input type="hidden" name="q" value="site:http://hexo.io">' +
+      '<input type="hidden" name="sitesearch" value="http://hexo.io">' +
       '</form>');
   });
 
@@ -32,7 +32,7 @@ describe('search_form', function(){
     searchForm({button: true, text: 'Find'}).should.eql('<form action="//google.com/search" method="get" accept-charset="UTF-8" class="search-form">' +
       '<input type="search" name="q" results="0" class="search-form-input" placeholder="Find">' +
       '<button type="submit" class="search-form-submit">Find</button>' +
-      '<input type="hidden" name="q" value="site:http://hexo.io">' +
+      '<input type="hidden" name="sitesearch" value="http://hexo.io">' +
       '</form>');
   });
 
@@ -40,7 +40,7 @@ describe('search_form', function(){
     searchForm({button: 'Go', text: 'Find'}).should.eql('<form action="//google.com/search" method="get" accept-charset="UTF-8" class="search-form">' +
       '<input type="search" name="q" results="0" class="search-form-input" placeholder="Find">' +
       '<button type="submit" class="search-form-submit">Go</button>' +
-      '<input type="hidden" name="q" value="site:http://hexo.io">' +
+      '<input type="hidden" name="sitesearch" value="http://hexo.io">' +
       '</form>');
   });
 });
