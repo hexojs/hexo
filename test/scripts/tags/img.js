7dcc719ebab8ceb3f306d546a1eb985a28f4b82c
liuhongjiang <andrew.lhj@gmail.com>
2015-08-11 13:41:48 +0800

    Support root path configure for img tag
    Current img tag doesn't prepend root path if it's not a slash. With
    this fix, the root path will be prepend to the img src if it's not a
    slash.
    This fix only works for the hexo tags, does not work for the markdown
    syntax of links and images. The issues of markdown links and images should
    be fixed in the hexo-renderer-marked module.
    
    Resolves: #1440

--- test/scripts/tags/img.js
+++ test/scripts/tags/img.js
@@ -1,10 +1,17 @@
 'use strict';
 
+var pathFn = require('path');
 var cheerio = require('cheerio');
 var should = require('chai').should(); // eslint-disable-line
 
 describe('img', function() {
-  var img = require('../../../lib/plugins/tag/img');
+  var Hexo = require('../../../lib/hexo');
+  var hexo = new Hexo(pathFn.join(__dirname, 'img_test'));
+  var img = require('../../../lib/plugins/tag/img')(hexo);
+
+  before(function() {
+    return hexo.init();
+  });
 
   it('src', function() {
     var $ = cheerio.load(img(['http://placekitten.com/200/300']));
@@ -13,9 +20,13 @@ describe('img', function() {
   });
 
   it('internal src', function() {
+    hexo.config.root = '/';
     var $ = cheerio.load(img(['/images/test.jpg']));
-
     $('img').attr('src').should.eql('/images/test.jpg');
+
+    hexo.config.root = '/root/';
+    $ = cheerio.load(img(['/images/test.jpg']));
+    $('img').attr('src').should.eql('/root/images/test.jpg');
   });
 
   it('class + src', function() {
@@ -26,9 +37,14 @@ describe('img', function() {
   });
 
   it('class + internal src', function() {
+    hexo.config.root = '/';
     var $ = cheerio.load(img('left /images/test.jpg'.split(' ')));
-
     $('img').attr('src').should.eql('/images/test.jpg');
+    $('img').attr('class').should.eql('left');
+
+    hexo.config.root = '/root/';
+    $ = cheerio.load(img('left /images/test.jpg'.split(' ')));
+    $('img').attr('src').should.eql('/root/images/test.jpg');
     $('img').attr('class').should.eql('left');
   });
 
@@ -40,9 +56,14 @@ describe('img', function() {
   });
 
   it('multiple classes + internal src', function() {
+    hexo.config.root = '/';
     var $ = cheerio.load(img('left top /images/test.jpg'.split(' ')));
-
     $('img').attr('src').should.eql('/images/test.jpg');
+    $('img').attr('class').should.eql('left top');
+
+    hexo.config.root = '/root/';
+    $ = cheerio.load(img('left top /images/test.jpg'.split(' ')));
+    $('img').attr('src').should.eql('/root/images/test.jpg');
     $('img').attr('class').should.eql('left top');
   });
 
