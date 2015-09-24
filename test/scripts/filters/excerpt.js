ab0f5ec51cd19f4f1312e9fb142c3764797d4e64
amobiz <amobiz.tw+github@gmail.com>
2015-09-24 13:56:17 +0800

    fix: #1451: more cannot work in almost all themes

--- test/scripts/filters/excerpt.js
+++ test/scripts/filters/excerpt.js
@@ -25,34 +25,67 @@ describe('Excerpt', function() {
   });
 
   it('with <!-- more -->', function() {
-    var content = [
-      'foo',
-      'bar',
-      '<!-- more -->',
-      'baz'
-    ].join('\n');
 
-    var data = {
-      content: content
-    };
+    _moreCases().forEach(_test);
 
-    excerpt(data);
+    function _moreCases() {
+      var template = '<!--{{lead}}more{{tail}}-->';
+      // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp#Special_characters_meaning_in_regular_expressions
+      var spaces = ' \f\n\r\t\v\u00a0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000\ufeff';
+      var cases = [];
+      var more, lead, tail, s, e;
 
-    data.content.should.eql([
-      'foo',
-      'bar',
-      '<a id="more"></a>',
-      'baz'
-    ].join('\n'));
+      for (var i = 0; i < spaces.length; ++i) {
+        lead = spaces[i];
+        for (var k = 0; k < spaces.length; ++k) {
+          tail = spaces[k];
+          s = '';
+          for (var m = 0; m < 3; ++m) {
+            e = '';
+            for (var n = 0; n < 3; ++n) {
+              more = template.replace('{{lead}}', s).replace('{{tail}}', e);
+              cases.push(more);
+              e += tail;
+            }
 
-    data.excerpt.should.eql([
-      'foo',
-      'bar'
-    ].join('\n'));
+            s += lead;
+          }
+        }
+      }
 
-    data.more.should.eql([
-      'baz'
-    ].join('\n'));
+      return cases;
+    }
+
+    function _test(more) {
+      var content = [
+        'foo',
+        'bar',
+        more,
+        'baz'
+      ].join('\n');
+
+      var data = {
+        content: content
+      };
+
+      excerpt(data);
+
+      data.content.should.eql([
+        'foo',
+        'bar',
+        '<a id="more"></a>',
+        'baz'
+      ].join('\n'));
+
+      data.excerpt.should.eql([
+        'foo',
+        'bar'
+      ].join('\n'));
+
+      data.more.should.eql([
+        'baz'
+      ].join('\n'));
+    }
   });
 
   it('multiple <!-- more -->', function() {
