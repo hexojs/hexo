50c264927f6d2a54eaa6ff9fc1c374cd62154281
leesei <leesei@gmail.com>
2016-02-26 15:57:12 +0800

    lint(tags/code): make linter happy
     Changes to be committed:
    	modified:   lib/plugins/tag/code.js
    	modified:   test/scripts/tags/code.js

--- test/scripts/tags/code.js
+++ test/scripts/tags/code.js
@@ -113,17 +113,17 @@ describe('code', function() {
 
   it('mark', function() {
     var source = [
-      "const http = require('http');",
-      "",
-      "const hostname = '127.0.0.1';",
-      "const port = 1337;",
-      "",
-      "http.createServer((req, res) => {",
-      "  res.writeHead(200, { 'Content-Type': 'text/plain' });",
-      "  res.end('Hello World\n');",
-      "}).listen(port, hostname, () => {",
-      "  console.log(`Server running at http://${hostname}:${port}/`);",
-      "});"
+      'const http = require(\'http\');',
+      '',
+      'const hostname = \'127.0.0.1\';',
+      'const port = 1337;',
+      '',
+      'http.createServer((req, res) => {',
+      '  res.writeHead(200, { \'Content-Type\': \'text/plain\' });',
+      '  res.end(\'Hello World\n\');',
+      '}).listen(port, hostname, () => {',
+      '  console.log(`Server running at http://${hostname}:${port}/`);',
+      '});'
     ].join('\n');
     var result = code('mark:1,7-8,10', source);
     console.log(result);
