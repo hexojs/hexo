d27b704258842acffe09104b47e4bcc265d5e229
Daniel J. Summers <daniel.summers.2415@gmail.com>
2017-09-05 17:09:32 -0500

    848 hierarchical categories (#2734)
    * Hierarchical categories can be assigned
    
    The code still needs some cleanup, but the new test passes without
    breaking the old ones.
    
    * more tests, cleanup
    
    added tests for category list and front-matter processor to include
    multiple categories; cleaned up Post.setCategories() code
    
    * -whitespace
    
    * --no-fat-arrow
    
    CodeClimate didn't like 'em...
    
    * Added test for dupe parents
    
    ensure we do not create a duplicate post/category xref for duplicate
    parent categories

--- test/scripts/models/post.js
+++ test/scripts/models/post.js
@@ -313,6 +313,44 @@ describe('Post', () => {
     }).finally(() => Post.removeById(id));
   });
 
+  it('setCategories() - multiple hierarchies', () => Post.insert({
+    source: 'foo.md',
+    slug: 'bar'
+  }).then(post => post.setCategories([['foo', '', 'bar'], '', 'baz'])
+    .thenReturn(Post.findById(post._id))).then(post => {
+    var cats = post.categories.toArray();
+
+    // There should have been 3 categories set; blanks eliminated
+    cats.should.have.lengthOf(3);
+
+    // Category 1 should be foo, no parent
+    cats[0].name.should.eql('foo');
+    should.not.exist(cats[0].parent);
+
+    // Category 2 should be bar, foo as parent
+    cats[1].name.should.eql('bar');
+    cats[1].parent.should.eql(cats[0]._id);
+
+    // Category 3 should be baz, no parent
+    cats[2].name.should.eql('baz');
+    should.not.exist(cats[2].parent);
+
+    return Post.removeById(post._id);
+  }));
+
+  it('setCategories() - multiple hierarchies (dedupes repeated parent)', () => Post.insert({
+    source: 'foo.md',
+    slug: 'bar'
+  }).then(post => post.setCategories([['foo', 'bar'], ['foo', 'baz']])
+    .thenReturn(Post.findById(post._id))).then(post => {
+    var cats = post.categories.toArray();
+
+    // There should have been 3 categories set (foo is dupe)
+    cats.should.have.lengthOf(3);
+
+    return Post.removeById(post._id);
+  }));
+
   it('remove PostTag references when a post is removed', () => Post.insert({
     source: 'foo.md',
     slug: 'bar'
