eb8f618507845e1e2e9d98b9be8bceb82cff744f
Mathew Goldsborough <mathew.goldsborough@foreveroceans.com>
2018-04-16 10:52:43 -1000

    added command line option `output` to write db.json and _multiconfig.yml to a path provided

--- test/scripts/hexo/multi_config_path.js
+++ test/scripts/hexo/multi_config_path.js
@@ -14,6 +14,22 @@ describe('config flag handling', () => {
 
   function ConsoleReader() {
     this.reader = [];
+    this.d = function(...args) {
+      var type = 'debug';
+      var message = '';
+      for (var i = 0; i < args.length;) {
+        message += args[i];
+        if (++i < args.length) {
+          message += ' ';
+        }
+      }
+
+      this.reader.push({
+        type,
+        msg: message
+      });
+    }.bind(this);
+
     this.i = function(...args) {
       var type = 'info';
       var message = '';
@@ -145,15 +161,15 @@ describe('config flag handling', () => {
 
     hexo.log.reader[0].type.should.eql('info');
     hexo.log.reader[0].msg.should.eql('Config based on 2 files');
-    hexo.log.reader[3].type.should.eql('warning');
-    hexo.log.reader[3].msg.should.eql('Config file notafile.yml not found.');
-    hexo.log.reader[4].type.should.eql('info');
-    hexo.log.reader[4].msg.should.eql('Config based on 1 files');
+    hexo.log.reader[6].type.should.eql('warning');
+    hexo.log.reader[6].msg.should.eql('Config file notafile.yml not found.');
+    hexo.log.reader[7].type.should.eql('info');
+    hexo.log.reader[7].msg.should.eql('Config based on 1 files');
     // because who cares about grammar anyway?
 
     mcp(base, 'notafile.yml,alsonotafile.json').should.not.eql(combinedPath);
-    hexo.log.reader[7].type.should.eql('error');
-    hexo.log.reader[7].msg.should.eql('No config files found.'
+    hexo.log.reader[11].type.should.eql('error');
+    hexo.log.reader[11].msg.should.eql('No config files found.'
                                      + ' Using _config.yml.');
   });
 
@@ -204,5 +220,29 @@ describe('config flag handling', () => {
     config.author.should.eql('foo');
     config.favorites.food.should.eql('sushi');
     config.type.should.eql('dinosaur');
+  });
+
+  it('write multiconfig to specified path', () => {
+    let outputPath = '/tmp';
+    let combinedPath = pathFn.join(outputPath, '_multiconfig.yml');
+
+    mcp(base, 'test1.yml', outputPath).should.not.eql(combinedPath);
+    mcp(base, 'test1.yml,test2.yml', outputPath).should.eql(combinedPath);
+    mcp(base, 'test1.yml,test1.json', outputPath).should.eql(combinedPath);
+    mcp(base, 'test1.json,test2.json', outputPath).should.eql(combinedPath);
+    mcp(base, 'notafile.yml,test1.json', outputPath).should.eql(combinedPath);
+    mcp(base, 'notafile.yml,alsonotafile.json', outputPath).should.not.eql(combinedPath);
+
+    hexo.log.reader[1].type.should.eql('debug');
+    hexo.log.reader[1].msg.should.eql('Writing _multiconfig.yml to /tmp/_multiconfig.yml');
+    hexo.log.reader[2].type.should.eql('info');
+    hexo.log.reader[2].msg.should.eql('Config based on 2 files');
+    hexo.log.reader[6].type.should.eql('warning');
+    hexo.log.reader[6].msg.should.eql('Config file notafile.yml not found.');
+    hexo.log.reader[7].type.should.eql('info');
+    hexo.log.reader[7].msg.should.eql('Config based on 1 files');
+    hexo.log.reader[11].type.should.eql('error');
+    hexo.log.reader[11].msg.should.eql('No config files found.'
+                                     + ' Using _config.yml.');
   });
 });
