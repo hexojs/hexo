fffafac27de8151d28b7c170991ad2aea8ee1d16
curbengh <43627182+curbengh@users.noreply.github.com>
2019-09-20 19:30:41 +0100

    test(link_tag): urls now have trailing slash

--- test/scripts/tags/link.js
+++ test/scripts/tags/link.js
@@ -8,20 +8,20 @@ describe('link', () => {
   it('text + url', () => {
     const $ = cheerio.load(link('Click here to Google https://google.com'.split(' ')));
 
-    $('a').attr('href').should.eql('https://google.com');
+    $('a').attr('href').should.eql('https://google.com/');
     $('a').html().should.eql('Click here to Google');
   });
 
   it('text + url + external', () => {
     let $ = cheerio.load(link('Click here to Google https://google.com true'.split(' ')));
 
-    $('a').attr('href').should.eql('https://google.com');
+    $('a').attr('href').should.eql('https://google.com/');
     $('a').html().should.eql('Click here to Google');
     $('a').attr('target').should.eql('_blank');
 
     $ = cheerio.load(link('Click here to Google https://google.com false'.split(' ')));
 
-    $('a').attr('href').should.eql('https://google.com');
+    $('a').attr('href').should.eql('https://google.com/');
     $('a').html().should.eql('Click here to Google');
     $('a').attr('title').should.eql('');
     $('a').attr('target').should.eql('');
@@ -30,7 +30,7 @@ describe('link', () => {
   it('text + url + title', () => {
     const $ = cheerio.load(link('Click here to Google https://google.com Google link'.split(' ')));
 
-    $('a').attr('href').should.eql('https://google.com');
+    $('a').attr('href').should.eql('https://google.com/');
     $('a').html().should.eql('Click here to Google');
     $('a').attr('title').should.eql('Google link');
   });
@@ -38,14 +38,14 @@ describe('link', () => {
   it('text + url + external + title', () => {
     let $ = cheerio.load(link('Click here to Google https://google.com true Google link'.split(' ')));
 
-    $('a').attr('href').should.eql('https://google.com');
+    $('a').attr('href').should.eql('https://google.com/');
     $('a').html().should.eql('Click here to Google');
     $('a').attr('target').should.eql('_blank');
     $('a').attr('title').should.eql('Google link');
 
     $ = cheerio.load(link('Click here to Google https://google.com false Google link'.split(' ')));
 
-    $('a').attr('href').should.eql('https://google.com');
+    $('a').attr('href').should.eql('https://google.com/');
     $('a').html().should.eql('Click here to Google');
     $('a').attr('target').should.eql('');
     $('a').attr('title').should.eql('Google link');
