c60486465c743f5610799e5b96790fe8cf27044a
curbengh <43627182+curbengh@users.noreply.github.com>
2019-09-24 13:45:36 +0100

    test: remove console.log

--- test/scripts/filters/external_link.js
+++ test/scripts/filters/external_link.js
@@ -4,7 +4,6 @@ describe('External link', () => {
   const Hexo = require('../../../lib/hexo');
   const hexo = new Hexo();
   const externalLink = require('../../../lib/plugins/filter/after_render/external_link').bind(hexo);
-  console.log(typeof externalLink);
 
   hexo.config.external_link = true;
   hexo.config.url = 'https://example.com';
