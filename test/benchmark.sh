08ae1889f1b4ab6bda8386196994ba081189d7fd
SukkaW <isukkaw@gmail.com>
2019-10-19 00:57:15 +0800

    ci: format benchmark result output

--- test/benchmark.sh
+++ test/benchmark.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+_SUBSTRUCTION () {
+    echo | awk "{print $1-$2}"
+}
+
+LOG_TABLE () {
+    time_begin=$(date +%s -d "$(awk '/.*DEBUG Hexo version/{print $1}' build.log)")
+    time_process_start=$(date +%s.%3N -d "$(awk '/.*INFO  Start processing/{print $1}' build.log)")
+    time_render_start=$(date +%s.%3N -d "$(awk '/.*INFO  Files loaded in/{print $1}' build.log)")
+    time_render_finish=$(date +%s.%3N -d "$(awk '/.*INFO.*generated in /{print $1}' build.log)")
+    time_database_saved=$(date +%s.%3N -d "$(awk '/.*DEBUG Database saved/{print $1}' build.log)")
+
+    echo "Load Plugin/Scripts/Database $(_SUBSTRUCTION $time_process_start $time_begin)\nProcess Source $(_SUBSTRUCTION $time_render_start $time_process_start)\nRender Files $(_SUBSTRUCTION $time_render_finish $time_render_start)\nSave Database $(_SUBSTRUCTION $time_database_saved $time_render_finish)" | awk '{printf "| %-28s | %7.3fs |\n",$1" "$2,$3"s"}'
+}
+
 echo "============== Hexo Benchmark =============="
 
 echo "- Set up dummy Hexo site"
@@ -19,32 +33,24 @@ git clone https://github.com/SukkaLab/hexo-many-posts.git source/_posts/hexo-man
 rm -rf source/_posts/hexo-many-posts/.git/
 
 echo "- Replace node_modules/hexo"
+rm -rf node_modules/hexo
 ln -sf $TRAVIS_BUILD_DIR node_modules/hexo
 
 echo "- Start test run"
 
 echo "------------- Cold processing --------------"
 npx --no-install hexo g --debug > build.log
-cat build.log | grep "Hexo version"
-cat build.log | grep "Start processing"
-cat build.log | grep "loaded in"
-cat build.log | grep "generated in"
-cat build.log | grep "Database saved"
+LOG_TABLE
+
 echo "-------------- Hot processing --------------"
 npx --no-install hexo g --debug > build.log
-cat build.log | grep "Hexo version"
-cat build.log | grep "Start processing"
-cat build.log | grep "loaded in"
-cat build.log | grep "generated in"
-cat build.log | grep "Database saved"
+LOG_TABLE
+
 echo "--------- Another Cold processing ----------"
 npx --no-install hexo clean > build.log
 npx --no-install hexo g --debug > build.log
-cat build.log | grep "Hexo version"
-cat build.log | grep "Start processing"
-cat build.log | grep "loaded in"
-cat build.log | grep "generated in"
-cat build.log | grep "Database saved"
+LOG_TABLE
+
 rm -rf build.log
 
 cd $TRAVIS_BUILD_DIR
