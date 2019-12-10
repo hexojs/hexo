c19096b0a8deb824a62733d8aa3dcf2568c33efb
curbengh <43627182+curbengh@users.noreply.github.com>
2019-12-11 00:13:15 +1030

    test(open_graph): add default locale (#3924)

--- test/scripts/helpers/open_graph.js
+++ test/scripts/helpers/open_graph.js
@@ -36,6 +36,7 @@ describe('open_graph', () => {
         meta({property: 'og:title', content: hexo.config.title}),
         meta({property: 'og:url'}),
         meta({property: 'og:site_name', content: hexo.config.title}),
+        meta({property: 'og:locale', content: 'en_US'}),
         meta({property: 'article:published_time', content: post.date.toISOString()}),
         meta({property: 'article:modified_time', content: post.updated.toISOString()}),
         meta({property: 'article:author', content: hexo.config.author}),
