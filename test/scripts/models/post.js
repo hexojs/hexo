ca7d55f0a03beaab0c71de5c5c4fc2578d06c61f
curbengh <43627182+curbengh@users.noreply.github.com>
2019-12-10 06:03:08 +0000

    fix(permalink): remove unrelated hash variable

--- test/scripts/models/post.js
+++ test/scripts/models/post.js
@@ -39,7 +39,6 @@ describe('Post', () => {
       should.not.exist(data.content);
       should.not.exist(data.excerpt);
       should.not.exist(data.more);
-      should.not.exist(data.hash);
 
       return Post.removeById(data._id);
     });
