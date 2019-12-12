d217b8068ce7aa97b82cb83b4d1f529ea0ae822f
curbengh <43627182+curbengh@users.noreply.github.com>
2019-12-12 03:27:36 +0000

    ci(benchmark): check runtime and output lines

--- test/benchmark.sh
+++ test/benchmark.sh
@@ -9,13 +9,6 @@ _MESSAGE_FORMATTER () {
 }
 
 LOG_TABLE () {
-    if [ $? != 0 ]; then
-        echo "--------------------------------------------"
-        echo -e "\033[41;37m !! Build failed !! \033[0m"
-        cat build.log
-        exit 1
-    fi
-
     time_begin=$(date +%s -d "$(awk '/.*DEBUG Hexo version/{print $1}' build.log)")
     time_process_start=$(date +%s.%3N -d "$(awk '/.*INFO  Start processing/{print $1}' build.log)")
     time_render_start=$(date +%s.%3N -d "$(awk '/.*INFO  Files loaded in/{print $1}' build.log)")
@@ -32,6 +25,14 @@ LOG_TABLE () {
     echo "Memory Usage(RSS) $(echo | awk "{printf \"%.3f\", $memory_usage/1024}")MB" | _MESSAGE_FORMATTER
 
     total_time=$(_SUBSTRUCTION $time_database_saved $time_begin | xargs -0 printf "%.0f")
+    line_number=$(wc -l build.log | cut -d" " -f1)
+
+if [ $total_time -lt 10 ] || ([ $line_number -lt 300 ] && [ $1 != "HOT" ]); then
+        echo "--------------------------------------------"
+        echo -e "\033[41;37m !! Build failed !! \033[0m"
+        cat build.log
+        exit 1
+    fi
 
     if [ $total_time -gt 40 ]; then
         echo "--------------------------------------------"
@@ -70,7 +71,7 @@ LOG_TABLE
 
 echo "-------------- Hot processing --------------"
 { /usr/bin/time -v npx --no-install hexo g --debug > build.log 2>&1 ; } 2> build.log
-LOG_TABLE
+LOG_TABLE "HOT"
 
 echo "--------- Another Cold processing ----------"
 npx --no-install hexo clean > build.log
