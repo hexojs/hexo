6a9297812f1831cf8d318368b78b11b2da8c2d29
curbengh <43627182+curbengh@users.noreply.github.com>
2019-12-14 06:41:18 +0000

    refactor(test-filters-render_post): async/await

--- test/scripts/filters/render_post.js
+++ test/scripts/filters/render_post.js
@@ -9,60 +9,57 @@ describe('Render post', () => {
   const Page = hexo.model('Page');
   const renderPost = require('../../../lib/plugins/filter/before_generate/render_post').bind(hexo);
 
-  before(() => hexo.init().then(() => hexo.loadPlugin(require.resolve('hexo-renderer-marked'))));
+  before(async() => {
+    await hexo.init();
+    await hexo.loadPlugin(require.resolve('hexo-renderer-marked'));
+  });
 
-  it('post', () => {
-    let id;
-
-    return Post.insert({
+  it('post', async() => {
+    let post = await Post.insert({
       source: 'foo.md',
       slug: 'foo',
       _content: fixture.content
-    }).then(post => {
-      id = post._id;
-      return renderPost();
-    }).then(() => {
-      const post = Post.findById(id);
-      post.content.trim().should.eql(fixture.expected);
-
-      return post.remove();
     });
+
+    const id = post._id;
+    await renderPost();
+
+    post = Post.findById(id);
+    post.content.trim().should.eql(fixture.expected);
+
+    post.remove();
   });
 
-  it('page', () => {
-    let id;
-
-    return Page.insert({
+  it('page', async() => {
+    let page = await Page.insert({
       source: 'foo.md',
       path: 'foo.html',
       _content: fixture.content
-    }).then(page => {
-      id = page._id;
-      return renderPost();
-    }).then(() => {
-      const page = Page.findById(id);
-      page.content.trim().should.eql(fixture.expected);
-
-      return page.remove();
     });
+
+    const id = page._id;
+    await renderPost();
+
+    page = Page.findById(id);
+    page.content.trim().should.eql(fixture.expected);
+
+    page.remove();
   });
 
-  it('use data variables', () => {
-    let id;
-
-    return Page.insert({
+  it('use data variables', async() => {
+    let page = await Page.insert({
       source: 'foo.md',
       path: 'foo.html',
       _content: '<p>Hello {{site.data.foo.name}}</p>'
-    }).then(page => {
-      id = page._id;
-      return renderPost({foo: {name: 'Hexo'}});
-    }).then(() => {
-      const page = Page.findById(id);
-      page.content.trim().should.eql('<p>Hello Hexo</p>');
-
-      return page.remove();
     });
+
+    const id = page._id;
+    await renderPost({foo: {name: 'Hexo'}});
+
+    page = Page.findById(id);
+    page.content.trim().should.eql('<p>Hello Hexo</p>');
+
+    page.remove();
   });
 
 });
