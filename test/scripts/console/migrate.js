d7a45889e06d937b04a0cb8c1e29e7a7c199c66f
curbengh <43627182+curbengh@users.noreply.github.com>
2019-12-14 06:06:55 +0000

    refactor(test-console-migrate): async/await

--- test/scripts/console/migrate.js
+++ test/scripts/console/migrate.js
@@ -7,7 +7,7 @@ describe('migrate', () => {
   const hexo = new Hexo(__dirname, {silent: true});
   const migrate = require('../../../lib/plugins/console/migrate').bind(hexo);
 
-  it('default', () => {
+  it('default', async() => {
     const migrator = sinon.spy(args => {
       args.foo.should.eql(1);
       args.bar.should.eql(2);
@@ -15,8 +15,7 @@ describe('migrate', () => {
 
     hexo.extend.migrator.register('test', migrator);
 
-    return migrate({_: ['test'], foo: 1, bar: 2}).then(() => {
-      migrator.calledOnce.should.be.true;
-    });
+    await migrate({_: ['test'], foo: 1, bar: 2});
+    migrator.calledOnce.should.eql(true);
   });
 });
