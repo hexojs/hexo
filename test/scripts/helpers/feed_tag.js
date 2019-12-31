527bf11eb929f2375af3a98123f4cea4c9773f46
curbengh <43627182+curbengh@users.noreply.github.com>
2019-12-31 04:51:27 +0000

    feat(feed_tag): support parsing config.feed

--- test/scripts/helpers/feed_tag.js
+++ test/scripts/helpers/feed_tag.js
@@ -1,26 +1,86 @@
 'use strict';
 
 describe('feed_tag', () => {
-  const Hexo = require('../../../lib/hexo');
-  const hexo = new Hexo(__dirname);
-
   const ctx = {
-    config: hexo.config
+    config: {
+      title: 'Hexo',
+      url: 'http://yoursite.com',
+      root: '/',
+      feed: {}
+    }
   };
+
+  beforeEach(() => { ctx.config.feed = {}; });
 
   ctx.url_for = require('../../../lib/plugins/helper/url_for').bind(ctx);
 
   const feed = require('../../../lib/plugins/helper/feed_tag').bind(ctx);
 
   it('path', () => {
-    feed('atom.xml').should.eql('<link rel="alternate" href="/atom.xml" title="Hexo">');
+    feed('atom.xml').should.eql('<link rel="alternate" href="/atom.xml" title="Hexo" type="application/atom+xml">');
   });
 
   it('title', () => {
-    feed('atom.xml', {title: 'RSS Feed'}).should.eql('<link rel="alternate" href="/atom.xml" title="RSS Feed">');
+    feed('atom.xml', {title: 'RSS Feed'}).should.eql('<link rel="alternate" href="/atom.xml" title="RSS Feed" type="application/atom+xml">');
   });
 
   it('type', () => {
-    feed('rss.xml', {type: 'rss'}).should.eql('<link rel="alternate" href="/rss.xml" title="Hexo">');
+    feed('rss.xml', {type: 'rss'}).should.eql('<link rel="alternate" href="/rss.xml" title="Hexo" type="application/rss+xml">');
+  });
+
+  it('invalid input - number', () => {
+    try {
+      feed(123);
+    } catch (err) {
+      err.message.should.eql('path must be a string!');
+    }
+  });
+
+  it('invalid input - undefined', () => {
+    ctx.config.feed = {};
+    const result = feed();
+    const typeOf = str => typeof str;
+
+    typeOf(result).should.eql('string');
+    result.length.should.eql(0);
+  });
+
+  it('feed - parse argument if available', () => {
+    ctx.config.feed = {
+      type: 'atom',
+      path: 'atom.xml'
+    };
+
+    feed('atomic.xml').should.eql('<link rel="alternate" href="/atomic.xml" title="Hexo" type="application/atom+xml">');
+  });
+
+  it('feed - atom', () => {
+    ctx.config.feed = {
+      type: 'atom',
+      path: 'atom.xml'
+    };
+
+    feed().should.eql('<link rel="alternate" href="/atom.xml" title="Hexo" type="application/atom+xml">');
+  });
+
+  it('feed - rss2', () => {
+    ctx.config.feed = {
+      type: 'rss2',
+      path: 'rss.xml'
+    };
+
+    feed().should.eql('<link rel="alternate" href="/rss.xml" title="Hexo" type="application/rss+xml">');
+  });
+
+  it('feed - rss2', () => {
+    ctx.config.feed = {
+      type: ['atom', 'rss2'],
+      path: ['atom.xml', 'rss.xml']
+    };
+
+    feed().should.eql([
+      '<link rel="alternate" href="/atom.xml" title="Hexo" type="application/atom+xml">',
+      '<link rel="alternate" href="/rss.xml" title="Hexo" type="application/rss+xml">'
+    ].join(''));
   });
 });
