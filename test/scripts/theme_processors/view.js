a2b363d8c10cd9d7f7e62ef87ec4b1e30e7bff0d
curbengh <43627182+curbengh@users.noreply.github.com>
2020-01-01 05:06:08 +0000

    refactor(test-theme_processors-view): destructure

--- test/scripts/theme_processors/view.js
+++ test/scripts/theme_processors/view.js
@@ -1,40 +1,40 @@
 'use strict';
 
-const pathFn = require('path');
-const fs = require('hexo-fs');
+const { join } = require('path');
+const { mkdirs, rmdir, unlink, writeFile } = require('hexo-fs');
 const Promise = require('bluebird');
 
 describe('view', () => {
   const Hexo = require('../../../lib/hexo');
-  const hexo = new Hexo(pathFn.join(__dirname, 'view_test'), {silent: true});
+  const hexo = new Hexo(join(__dirname, 'view_test'), {silent: true});
   const processor = require('../../../lib/theme/processors/view');
   const process = Promise.method(processor.process.bind(hexo));
-  const themeDir = pathFn.join(hexo.base_dir, 'themes', 'test');
+  const themeDir = join(hexo.base_dir, 'themes', 'test');
 
   hexo.env.init = true;
 
   function newFile(options) {
-    const path = options.path;
+    const { path } = options;
 
     options.params = {path};
     options.path = 'layout/' + path;
-    options.source = pathFn.join(themeDir, options.path);
+    options.source = join(themeDir, options.path);
 
     return new hexo.theme.File(options);
   }
 
   before(async () => {
     await Promise.all([
-      fs.mkdirs(themeDir),
-      fs.writeFile(hexo.config_path, 'theme: test')
+      mkdirs(themeDir),
+      writeFile(hexo.config_path, 'theme: test')
     ]);
     await hexo.init();
   });
 
-  after(() => fs.rmdir(hexo.base_dir));
+  after(() => rmdir(hexo.base_dir));
 
   it('pattern', () => {
-    const pattern = processor.pattern;
+    const { pattern } = processor;
 
     pattern.match('layout/index.swig').path.should.eql('index.swig');
     should.not.exist(pattern.match('index.swig'));
@@ -53,18 +53,18 @@ describe('view', () => {
       type: 'create'
     });
 
-    await fs.writeFile(file.source, body);
+    await writeFile(file.source, body);
     await process(file);
     const view = hexo.theme.getView('index.swig');
 
     view.path.should.eql('index.swig');
-    view.source.should.eql(pathFn.join(themeDir, 'layout', 'index.swig'));
+    view.source.should.eql(join(themeDir, 'layout', 'index.swig'));
     view.data.should.eql({
       foo: 'bar',
       _content: 'test'
     });
     hexo.theme.removeView('index.swig');
-    fs.unlink(file.source);
+    unlink(file.source);
   });
 
   it('type: delete', async () => {
