77d677cd1a04c444fd68c1b4a7f57ca93d886f5e
curbengh <43627182+curbengh@users.noreply.github.com>
2020-01-01 03:26:25 +0000

    refactor(test-theme-theme): destructure

--- test/scripts/theme/theme.js
+++ test/scripts/theme/theme.js
@@ -1,23 +1,23 @@
 'use strict';
 
-const pathFn = require('path');
-const fs = require('hexo-fs');
+const { join } = require('path');
+const { mkdirs, rmdir, writeFile } = require('hexo-fs');
 const Promise = require('bluebird');
 
 describe('Theme', () => {
   const Hexo = require('../../../lib/hexo');
-  const hexo = new Hexo(pathFn.join(__dirname, 'theme_test'), {silent: true});
-  const themeDir = pathFn.join(hexo.base_dir, 'themes', 'test');
+  const hexo = new Hexo(join(__dirname, 'theme_test'), {silent: true});
+  const themeDir = join(hexo.base_dir, 'themes', 'test');
 
   before(async () => {
     await Promise.all([
-      fs.mkdirs(themeDir),
-      fs.writeFile(hexo.config_path, 'theme: test')
+      mkdirs(themeDir),
+      writeFile(hexo.config_path, 'theme: test')
     ]);
     hexo.init();
   });
 
-  after(() => fs.rmdir(hexo.base_dir));
+  after(() => rmdir(hexo.base_dir));
 
   it('getView()', () => {
     hexo.theme.setView('test.swig', '');
