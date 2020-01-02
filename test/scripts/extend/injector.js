2da849c9afff02b70296d431fa8d731d5edd16b1
SukkaW <isukkaw@gmail.com>
2020-01-02 23:55:13 +0800

    test(injector): bring up extend/injector

---
+++ test/scripts/extend/injector.js
@@ -0,0 +1,54 @@
+'use strict';
+
+describe('Injector', () => {
+  const Injector = require('../../../lib/extend/injector');
+
+  it('register() - entry is required', () => {
+    const i = new Injector();
+
+    // no name
+    try {
+      i.register();
+    } catch (err) {
+      err.should.be
+        .instanceOf(TypeError)
+        .property('message', 'entry is required');
+    }
+  });
+
+  it('register() - string', () => {
+    const i = new Injector();
+
+    const str = '<link rel="stylesheet" href="DPlayer.min.css" />';
+    i.register('head_start', str);
+
+    i.get('head_start').should.contains(str);
+  });
+
+  it('register() - function', () => {
+    const i = new Injector();
+
+    const fn = () => '<link rel="stylesheet" href="DPlayer.min.css" />';
+    i.register('head_start', fn);
+
+    i.get('head_start').should.contains(fn());
+  });
+
+  it('list()', () => {
+    const i = new Injector();
+
+    i.register('body_begin', '<script src="DPlayer.min.js"></script>');
+
+    [...i.list().body_begin].should.not.be.empty;
+  });
+
+  it('get()', () => {
+    const i = new Injector();
+    const str = '<script src="jquery.min.js"></script>';
+
+    i.register('body_end', str);
+
+    i.get('body_end').should.be.instanceOf(Array);
+    i.get('body_end').should.contains(str);
+  });
+});
