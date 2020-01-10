dbb0ba243b1880abb770f1611150170992865bf1
SukkaW <isukkaw@gmail.com>
2020-01-10 16:26:52 +0800

    feat(meta_generator_helper): utilize prop

--- test/scripts/helpers/meta_generator.js
+++ test/scripts/helpers/meta_generator.js
@@ -4,13 +4,19 @@ describe('meta_generator', () => {
   const Hexo = require('../../../lib/hexo');
   const hexo = new Hexo();
 
-  const metaGeneratorHelper = require('../../../lib/plugins/helper/meta_generator').bind(hexo);
+  const metaGeneratorHelper = require('../../../lib/plugins/helper/meta_generator')(hexo);
+
+  before(() => hexo.init());
 
   it('default', () => {
     const { version } = hexo;
     const versionType = typeof version;
 
+    should.not.exist(hexo.extend.helper.getProp('meta_generator'));
+
     versionType.should.not.eql('undefined');
     metaGeneratorHelper().should.eql(`<meta name="generator" content="Hexo ${version}">`);
+
+    hexo.extend.helper.getProp('meta_generator').should.eql(true);
   });
 });
