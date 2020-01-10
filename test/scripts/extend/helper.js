54251601d8852661625ef56000d3b3bfcc6f23ac
SukkaW <isukkaw@gmail.com>
2020-01-10 15:58:00 +0800

    feat(helper): add prop, setProp() & getProp()

--- test/scripts/extend/helper.js
+++ test/scripts/extend/helper.js
@@ -2,10 +2,13 @@
 
 describe('Helper', () => {
   const Helper = require('../../../lib/extend/helper');
+  let h;
+
+  beforeEach(() => {
+    h = new Helper();
+  });
 
   it('register()', () => {
-    const h = new Helper();
-
     // name, fn
     h.register('test', () => {});
 
@@ -31,18 +34,45 @@ describe('Helper', () => {
   });
 
   it('list()', () => {
-    const h = new Helper();
-
     h.register('test', () => {});
 
     h.list().should.have.keys(['test']);
   });
 
   it('get()', () => {
-    const h = new Helper();
-
     h.register('test', () => {});
 
     h.get('test').should.exist;
+  });
+
+  it('setProp()', () => {
+    // name prop
+    h.setProp('foo', 'bar');
+
+    h.prop.foo.should.eql('bar');
+
+    // no name
+    try {
+      h.setProp();
+    } catch (err) {
+      err.should.be
+        .instanceOf(TypeError)
+        .property('message', 'name is required');
+    }
+
+    // no prop
+    try {
+      h.setProp('test');
+    } catch (err) {
+      err.should.be
+        .instanceOf(TypeError)
+        .property('message', 'prop is required');
+    }
+  });
+
+  it('getProp()', () => {
+    h.setProp('foo', 'bar');
+
+    h.getProp('foo').should.eql('bar');
   });
 });
