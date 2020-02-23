d18af379cadf7ec85385e5e324b2d9cb5e2aebb0
MDLeom <43627182+curbengh@users.noreply.github.com>
2020-02-23 17:35:30 +1030

    refactor(test-console-migrate): async/await & destructure (#3961)
    * refactor(test-console-migrate): async/await
    
    * refactor(test-console-migrate): destructure
    
    * style: asyncArrow

--- test/scripts/console/migrate.js
+++ test/scripts/console/migrate.js
@@ -1,22 +1,21 @@
 'use strict';
 
-const sinon = require('sinon');
+const { spy } = require('sinon');
 
 describe('migrate', () => {
   const Hexo = require('../../../lib/hexo');
   const hexo = new Hexo(__dirname, {silent: true});
   const migrate = require('../../../lib/plugins/console/migrate').bind(hexo);
 
-  it('default', () => {
-    const migrator = sinon.spy(args => {
+  it('default', async () => {
+    const migrator = spy(args => {
       args.foo.should.eql(1);
       args.bar.should.eql(2);
     });
 
     hexo.extend.migrator.register('test', migrator);
 
-    return migrate({_: ['test'], foo: 1, bar: 2}).then(() => {
-      migrator.calledOnce.should.be.true;
-    });
+    await migrate({_: ['test'], foo: 1, bar: 2});
+    migrator.calledOnce.should.eql(true);
   });
 });
