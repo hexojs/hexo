e9dac14d31326e1e98f634ed045e806a70fe74e4
Seaoak <seaoak2003@gmail.com>
2020-03-11 16:23:30 +0900

    Add test cases

--- test/scripts/filters/indented_code_block.js
+++ test/scripts/filters/indented_code_block.js
@@ -35,7 +35,18 @@ describe('Indented code block', () => {
     data.content.should.eql('<!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->');
   });
 
-  it('indent', () => {
+  it('single tab indent', () => {
+    const data = {
+      content: [
+        ...code_raw
+      ].map(x => `\t${x}`).join('\n')
+    };
+
+    codeBlock(data);
+    data.content.should.eql('<!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->');
+  });
+
+  it('extra indent', () => {
     const indentCode = code_raw.map(line => '  ' + line);
 
     const data = {
@@ -64,5 +75,57 @@ describe('Indented code block', () => {
 
     codeBlock(data);
     data.content.should.eql('<!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->');
+  });
+
+  it('include quote mark as content', () => {
+    const code_raw = [
+      '> if (tired && night){',
+      '>   sleep();',
+      '> }'
+    ];
+    const code = code_raw.join('\n');
+
+    const data = {
+      content: [
+        ...code_raw
+      ].map(x => `    ${x}`).join('\n')
+    };
+
+    codeBlock(data);
+    data.content.should.eql('<!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->');
+  });
+
+  it('included by blockquote', () => {
+    const code_raw = [
+      'if (tired && night){',
+      '  sleep();',
+      '}'
+    ];
+    const code = code_raw.join('\n');
+
+    const code_cooked = [
+      ...code_raw
+    ].map(x => `    ${x}`);
+
+    const data = {
+      content: [
+        'aaa',
+        '',
+        ...code_cooked,
+        '',
+        'bbb'
+      ].map(x => `> ${x}`).join('\n')
+    };
+
+    const expected = [
+      '> aaa',
+      '> ',
+      '> <!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->',
+      '> ',
+      '> bbb'
+    ].join('\n');
+
+    codeBlock(data);
+    data.content.should.eql(expected);
   });
 });
