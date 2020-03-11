420aa21781d374e0b13cbb09e4cfe59c7c0e034c
Seaoak <seaoak2003@gmail.com>
2020-03-11 15:52:05 +0900

    Remove highlight() from test cases

--- test/scripts/filters/indented_code_block.js
+++ test/scripts/filters/indented_code_block.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const util = require('hexo-util');
-const defaultConfig = require('../../../lib/hexo/default_config');
 
 describe('Indented code block', () => {
   const Hexo = require('../../../lib/hexo');
@@ -15,50 +14,14 @@ describe('Indented code block', () => {
   ];
   const code = code_raw.join('\n');
 
-  function highlight(code, options) {
-    return util.highlight(code, options || {})
+  function wrap(code) {
+    const content = util.escapeHTML(code)
       .replace(/{/g, '&#123;')
       .replace(/}/g, '&#125;');
-  }
-
-  function wrap(code) {
-    return '<pre><code>' + code
-      .replace(/{/g, '&#123;')
-      .replace(/}/g, '&#125;')
-      + '</code></pre>';
+    return '<pre><code>' + content + '</code></pre>';
   }
 
   beforeEach(() => {
-    // Reset config
-    hexo.config.highlight = Object.assign({}, defaultConfig.highlight);
-  });
-
-  it('disabled', () => {
-    const content = [
-      ...code_raw
-    ].map(x => `    ${x}`).join('\n');
-
-    const data = {content};
-
-    hexo.config.highlight.enable = false;
-    codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->');
-  });
-
-  it('with no config (disabled)', () => {
-    const content = [
-      ...code_raw
-    ].map(x => `    ${x}`).join('\n');
-
-    const data = {content};
-
-    const oldConfig = hexo.config.highlight;
-    delete hexo.config.highlight;
-
-    codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->');
-
-    hexo.config.highlight = oldConfig;
   });
 
   it('default', () => {
@@ -69,33 +32,7 @@ describe('Indented code block', () => {
     };
 
     codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + highlight(code, {}) + ':hexoPostRenderEscape-->');
-  });
-
-  it('without language name', () => {
-    const data = {
-      content: [
-        ...code_raw
-      ].map(x => `    ${x}`).join('\n')
-    };
-
-    const expected = highlight(code);
-
-    codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + expected + ':hexoPostRenderEscape-->');
-  });
-
-  it('without language name - ignore tab character', () => {
-    const data = {
-      content: [
-        ...code_raw
-      ].map(x => `    ${x}`).join('\n')
-    };
-
-    const expected = highlight(code);
-
-    codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + expected + ':hexoPostRenderEscape-->');
+    data.content.should.eql('<!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->');
   });
 
   it('indent', () => {
@@ -107,86 +44,11 @@ describe('Indented code block', () => {
       ].map(x => `    ${x}`).join('\n')
     };
 
-    const expected = highlight(code, {
-    });
-
     codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + expected + ':hexoPostRenderEscape-->');
+    data.content.should.eql('<!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->');
   });
 
-  it('line number false', () => {
-    hexo.config.highlight.line_number = false;
-
-    const data = {
-      content: [
-        ...code_raw
-      ].map(x => `    ${x}`).join('\n')
-    };
-
-    const expected = highlight(code, {
-      gutter: false
-    });
-
-    codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + expected + ':hexoPostRenderEscape-->');
-  });
-
-  it('line number false, don`t first_line_number always1', () => {
-    hexo.config.highlight.line_number = false;
-    hexo.config.highlight.first_line_number = 'always1';
-
-    const data = {
-      content: [
-        ...code_raw
-      ].map(x => `    ${x}`).join('\n')
-    };
-
-    const expected = highlight(code, {
-      gutter: false
-    });
-
-    codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + expected + ':hexoPostRenderEscape-->');
-  });
-
-  it('line number false, don`t care first_line_number inilne', () => {
-    hexo.config.highlight.line_number = false;
-    hexo.config.highlight.first_line_number = 'inilne';
-
-    const data = {
-      content: [
-        ...code_raw
-      ].map(x => `    ${x}`).join('\n')
-    };
-
-    const expected = highlight(code, {
-      gutter: false
-    });
-
-    codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + expected + ':hexoPostRenderEscape-->');
-  });
-
-  it('line number true', () => {
-    hexo.config.highlight.line_number = true;
-
-    const data = {
-      content: [
-        ...code_raw
-      ].map(x => `    ${x}`).join('\n')
-    };
-
-    const expected = highlight(code, {
-      gutter: true
-    });
-
-    codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + expected + ':hexoPostRenderEscape-->');
-  });
-
-  it('tab replace', () => {
-    hexo.config.highlight.tab_replace = '  ';
-
+  it('include tab', () => {
     const code_raw = [
       'if (tired && night){',
       '\tsleep();',
@@ -200,26 +62,7 @@ describe('Indented code block', () => {
       ].map(x => `    ${x}`).join('\n')
     };
 
-    const expected = highlight(code, {
-      tab: '  '
-    });
-
     codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + expected + ':hexoPostRenderEscape-->');
-  });
-
-  it('wrap', () => {
-    hexo.config.highlight.wrap = false;
-
-    const data = {
-      content: [
-        ...code_raw
-      ].map(x => `    ${x}`).join('\n')
-    };
-
-    codeBlock(data);
-    data.content.should.eql('<!--hexoPostRenderEscape:' + highlight(code, {wrap: false }) + ':hexoPostRenderEscape-->');
-
-    hexo.config.highlight.wrap = true;
+    data.content.should.eql('<!--hexoPostRenderEscape:' + wrap(code) + ':hexoPostRenderEscape-->');
   });
 });
