f3720e34fede4ece342cb14dd4037dd2357e3afd
himself65 <himself65@outlook.com>
2020-07-06 20:19:46 +0800

    feat: add benchmark script

---
+++ test/profiling.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+echo "- Set up dummy Hexo site"
+cd $TRAVIS_BUILD_DIR
+cd ..
+git clone https://github.com/hexojs/hexo-theme-unit-test.git --depth=1 --quiet
+cd hexo-theme-unit-test
+
+echo "- Install hexo-theme-landscape"
+git clone https://github.com/hexojs/hexo-theme-landscape --depth=1 --quiet themes/landscape
+
+echo "- npm install"
+npm install --silent
+
+echo "- Import 500 posts"
+git clone https://github.com/SukkaLab/hexo-many-posts.git source/_posts/hexo-many-posts --depth=1 --quiet
+rm -rf source/_posts/hexo-many-posts/.git/
+
+echo "- Replace node_modules/hexo"
+rm -rf node_modules/hexo
+ln -sf $TRAVIS_BUILD_DIR node_modules/hexo
+
+echo "- Start test run"
+echo ""
+
+echo "--------------------------------------------"
+npx --no-install hexo clean > build.log
+
+echo ""
+echo "- Generating flamegraph..."
+
+npm install 0x --silent
+node ./node_modules/.bin/0x --output-dir "${TRAVIS_BUILD_DIR}/0x" -- node ./node_modules/.bin/hexo g > build.log 2>&1 ;
+
+echo "Flamegraph will be deployed to: https://${TRAVIS_COMMIT}-${TRAVIS_NODE_VERSION}-hexo.surge.sh/flamegraph.html"
+
+rm -rf build.log
+cd $TRAVIS_BUILD_DIR
