91ba73c2b877d27721c401991433c7721e39e9f2
MDLeom <43627182+curbengh@users.noreply.github.com>
2020-08-29 08:27:55 +0000

    refactor(test/scaffold): async/await

--- test/scripts/hexo/scaffold.js
+++ test/scripts/hexo/scaffold.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const pathFn = require('path');
-const Promise = require('bluebird');
 const fs = require('hexo-fs');
 
 describe('Scaffold', () => {
@@ -19,48 +18,54 @@ describe('Scaffold', () => {
 
   const testPath = pathFn.join(scaffoldDir, 'test.md');
 
-  before(() => hexo.init().then(() => fs.writeFile(testPath, testContent)));
+  before(async () => {
+    await hexo.init();
+    await fs.writeFile(testPath, testContent);
+  });
 
   after(() => fs.rmdir(scaffoldDir));
 
-  it('get() - file exists', () => scaffold.get('test').then(data => {
+  it('get() - file exists', async () => {
+    const data = await scaffold.get('test');
     data.should.eql(testContent);
-  }));
-
-  it('get() - normal scaffold', () => scaffold.get('normal').then(data => {
-    data.should.eql(scaffold.defaults.normal);
-  }));
-
-  it('set() - file exists', () => scaffold.set('test', 'foo').then(() => Promise.all([
-    fs.readFile(testPath),
-    scaffold.get('test')
-  ])).spread((file, data) => {
-    file.should.eql('foo');
-    data.should.eql('foo');
-    return fs.writeFile(testPath, testContent);
-  }));
-
-  it('set() - file does not exist', () => {
-    const testPath = pathFn.join(scaffoldDir, 'foo.md');
-
-    return scaffold.set('foo', 'bar').then(() => Promise.all([
-      fs.readFile(testPath),
-      scaffold.get('foo')
-    ])).spread((file, data) => {
-      file.should.eql('bar');
-      data.should.eql('bar');
-      return fs.unlink(testPath);
-    });
   });
 
-  it('remove() - file exist', () => scaffold.remove('test').then(() => Promise.all([
-    fs.exists(testPath),
-    scaffold.get('test')
-  ])).spread((exist, data) => {
+  it('get() - normal scaffold', async () => {
+    const data = await scaffold.get('normal');
+    data.should.eql(scaffold.defaults.normal);
+  });
+
+  it('set() - file exists', async () => {
+    await scaffold.set('test', 'foo');
+
+    const file = await fs.readFile(testPath);
+    const data = await scaffold.get('test');
+    file.should.eql('foo');
+    data.should.eql('foo');
+
+    await fs.writeFile(testPath, testContent);
+  });
+
+  it('set() - file does not exist', async () => {
+    const testPath = pathFn.join(scaffoldDir, 'foo.md');
+
+    await scaffold.set('foo', 'bar');
+    const file = await fs.readFile(testPath);
+    const data = await scaffold.get('foo');
+    file.should.eql('bar');
+    data.should.eql('bar');
+    await fs.unlink(testPath);
+  });
+
+  it('remove() - file exist', async () => {
+    await scaffold.remove('test');
+    const exist = await fs.exists(testPath);
+    const data = await scaffold.get('test');
     exist.should.be.false;
     should.not.exist(data);
-    return fs.writeFile(testPath, testContent);
-  }));
+
+    await fs.writeFile(testPath, testContent);
+  });
 
   it('remove() - file does not exist', () => scaffold.remove('foo'));
 });
