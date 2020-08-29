226d8b40a44abe14f2f050ffcd0993fc0d58c1a6
MDLeom <43627182+curbengh@users.noreply.github.com>
2020-08-29 08:33:39 +0000

    refactor(test/update_package): async/await

--- test/scripts/hexo/update_package.js
+++ test/scripts/hexo/update_package.js
@@ -13,52 +13,56 @@ describe('Update package.json', () => {
     hexo.env.init = false;
   });
 
-  it('package.json does not exist', () => updatePkg(hexo).then(() => {
+  it('package.json does not exist', async () => {
+    await updatePkg(hexo);
     hexo.env.init.should.be.false;
-  }));
+  });
 
-  it('package.json exists, but the version doesn\'t match', () => {
+  it('package.json exists, but the version doesn\'t match', async () => {
     const pkg = {
       hexo: {
         version: '0.0.1'
       }
     };
 
-    return fs.writeFile(packagePath, JSON.stringify(pkg)).then(() => updatePkg(hexo)).then(() => fs.readFile(packagePath)).then(content => {
-      JSON.parse(content).hexo.version.should.eql(hexo.version);
-      hexo.env.init.should.be.true;
+    await fs.writeFile(packagePath, JSON.stringify(pkg));
+    await updatePkg(hexo);
+    const content = await fs.readFile(packagePath);
+    JSON.parse(content).hexo.version.should.eql(hexo.version);
+    hexo.env.init.should.be.true;
 
-      return fs.unlink(packagePath);
-    });
+    await fs.unlink(packagePath);
   });
 
-  it('package.json exists, but don\'t have hexo data', () => {
+  it('package.json exists, but don\'t have hexo data', async () => {
     const pkg = {
       name: 'hexo',
       version: '0.0.1'
     };
 
-    return fs.writeFile(packagePath, JSON.stringify(pkg)).then(() => updatePkg(hexo)).then(() => fs.readFile(packagePath)).then(content => {
-      // Don't change the original package.json
-      JSON.parse(content).should.eql(pkg);
-      hexo.env.init.should.be.false;
+    await fs.writeFile(packagePath, JSON.stringify(pkg));
+    await updatePkg(hexo);
+    const content = await fs.readFile(packagePath);
+    // Don't change the original package.json
+    JSON.parse(content).should.eql(pkg);
+    hexo.env.init.should.be.false;
 
-      return fs.unlink(packagePath);
-    });
+    await fs.unlink(packagePath);
   });
 
-  it('package.json exists and everything is ok', () => {
+  it('package.json exists and everything is ok', async () => {
     const pkg = {
       hexo: {
         version: hexo.version
       }
     };
 
-    return fs.writeFile(packagePath, JSON.stringify(pkg)).then(() => updatePkg(hexo)).then(() => fs.readFile(packagePath)).then(content => {
-      JSON.parse(content).should.eql(pkg);
-      hexo.env.init.should.be.true;
+    await fs.writeFile(packagePath, JSON.stringify(pkg));
+    await updatePkg(hexo);
+    const content = await fs.readFile(packagePath);
+    JSON.parse(content).should.eql(pkg);
+    hexo.env.init.should.be.true;
 
-      return fs.unlink(packagePath);
-    });
+    await fs.unlink(packagePath);
   });
 });
