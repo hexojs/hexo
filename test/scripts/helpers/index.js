e349348866e097b752a0990bc88fa18f9ae88093
Alexander Wong <alex@udia.ca>
2020-11-27 12:03:51 -0700

    Ported over `escape_html` tests from hexo-util

--- test/scripts/helpers/index.js
+++ test/scripts/helpers/index.js
@@ -4,6 +4,7 @@ describe('Helpers', () => {
   require('./debug');
   require('./css');
   require('./date');
+  require('./escape_html');
   require('./favicon_tag');
   require('./feed_tag');
   require('./fragment_cache');
