cb19b2938d1f7882a4cb41a98974a3d673a63b45
Baoshuo Ren <i@baoshuo.ren>
2023-02-28 00:54:43 +0800

    feat: add an option to disable titlecase in post (#5156)

--- test/scripts/filters/titlecase.js
+++ test/scripts/filters/titlecase.js
@@ -22,4 +22,23 @@ describe('Titlecase', () => {
     titlecase(data);
     data.title.should.eql('Today Is a Good Day');
   });
+
+  it('enabled globally but disabled in a specify post', () => {
+    const title = 'Today is a good day';
+    const data = {title, titlecase: false};
+    hexo.config.titlecase = true;
+
+    titlecase(data);
+    data.title.should.eql('Today is a good day');
+  });
+
+  it('disabled globally but enabled in a specify post', () => {
+    const title = 'Today is a good day';
+    const data = {title, titlecase: true};
+    hexo.config.titlecase = false;
+
+    titlecase(data);
+    data.title.should.eql('Today Is a Good Day');
+  });
+
 });
