cc9518b24d62662d3476efe7379ab271ff2abaf9
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-18 11:02:53 +0700

    feat: add data type `before_post_render`

---
+++ lib/plugins/filter/before_post_render/dataType.ts
@@ -0,0 +1,12 @@
+/**
+ * before_post_render `data` parameter
+ *
+ * @example
+ * hexo.extend.filter.register('before_post_render', function(data){ console.log(data.content) })
+ */
+export interface extend_filter_before_post_render_data {
+  [key: string]: any;
+  content: string | null | undefined;
+  source: string;
+  config: import('../../../hexo')['config'];
+}
